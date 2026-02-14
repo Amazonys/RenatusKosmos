5efed2893825c4210c1737a36199df4d9b4bfcf4
Amyzonis <Amazonys@users.noreply.github.com>
2026-02-13 23:34:00 -0600

    Yuge changes

--- Development/Python/new.py
+++ Development/Python/new.py
@@ -9,8 +9,8 @@ import json
 import matplotlib.pyplot as plt
 
 #Import file
-RegionV = np.loadtxt("C:/Users/Amyzonis/Documents/My Games/TerraInvicta/AmyzTI/RegionV.txt", comments="#", delimiter=" ", unpack=False)#vertex from obj file
-RegionF = np.loadtxt("C:/Users/Amyzonis/Documents/My Games/TerraInvicta/AmyzTI/RegionF.txt", dtype="int", comments="#", delimiter=" ", unpack=False)#index from obj file
+RegionV = np.loadtxt("C:/Program Files (x86)/Steam/steamapps/common/Terra Invicta/Mods/Enabled/RenatusKosmos/Development/Python/RegionV.txt", comments="#", delimiter=" ", unpack=False)
+RegionF = np.loadtxt("C:/Program Files (x86)/Steam/steamapps/common/Terra Invicta/Mods/Enabled/RenatusKosmos/Development/Python/RegionF.txt", dtype="int", comments="#", delimiter=" ", unpack=False)
 RegionI = RegionF.flatten()
 
 
@@ -21,7 +21,7 @@ def RSPFill0(Lon, Lat):
     r = math.cos(math.radians(Lat))*R
     RSPX = -math.sin(math.radians(Lon))*r
     RSPZ = math.cos(math.radians(Lon))*r
-    string = '\n{"0 Vector3f data": \n{\n"0 float x": RSPX, \n"0 float y": RSPY, \n"0 float z": RSPZ\n}},'
+    string = '\n{\n"x": RSPX, \n"y": RSPY, \n"z": RSPZ\n},'
     string1 = string.replace("RSPX", str(RSPX))
     string2 = string1.replace("RSPY", str(RSPY))
     string3 = string2.replace("RSPZ", str(RSPZ))
@@ -35,15 +35,14 @@ def RSPFill1(Lon, Lat, Idx):
 
 def RSPFill2(Lon, Lat, Idx):
     string = RSPFill1(Lon, Lat, Idx)
-    string1 = '"0 Vector3Array regionSurfacePoints":  {\n"0 Array Array": [\n{"0 Vector3Array data":  {\n"0 vector data":  {\n"1 Array Array": ['
-    string2 = ']\n}\n}}\n]\n},'
+    string1 = '"regionSurfacePoints":  {\n"Array": [\n{\n"data": {\n"Array": ['
+    string2 = '\n]\n}\n}\n]\n},'
     string3 = string1+'\n'+string+'\n'+string2
     return string3
 
 RSPTbt = RSPFill2(RegionV[0: , 0], RegionV[0: , 1], RegionI)
 
 
-
-OutlineOutput = open("C:/Users/Amyzonis/Documents/My Games/TerraInvicta/AmyzTI/output.txt", "w")
+OutlineOutput = open("C:/Program Files (x86)/Steam/steamapps/common/Terra Invicta/Mods/Enabled/RenatusKosmos/Development/Python/output.txt", "w")
 OutlineOutput.write(RSPTbt)
 OutlineOutput.close()
